671366cea534bc901a55d89638555a038a44465c
iMoovSmoov <104241864+iMoovSmoov@users.noreply.github.com>
2025-06-02 02:27:43 -0700

    Fix react-query import to use @tanstack/react-query

--- src/components/providers.tsx
+++ src/components/providers.tsx
@@ -2,7 +2,7 @@
 
 import { SessionProvider } from 'next-auth/react'
 import { ThemeProvider } from 'next-themes'
-import { QueryClient, QueryClientProvider } from 'react-query'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
 export function Providers({ children }: { children: React.ReactNode }) {
